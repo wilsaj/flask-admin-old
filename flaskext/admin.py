7a0d6cccefe9f4fd68f8d25dad39519b61ed4fb3
andy wilson <wilson.andrew.j+github@gmail.com>
2011-03-12 08:49:49 -0600

    get flask admin up to speed w/SWIS admin

--- flaskext/admin.py
+++ flaskext/admin.py
@@ -10,23 +10,27 @@
 """
 from __future__ import absolute_import
 
+import inspect
 import types
 
-from flask import app, flash, g, Module, render_template, redirect, request, session, url_for
-from sqlalchemy.orm.exc import NoResultFound
+import sqlalchemy as sa
 import sqlalchemy.ext.declarative
-from wtforms.ext.sqlalchemy.orm import model_form
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from flask import app, flash, g, Module, render_template, redirect, request, session, url_for
+from flaskext.sqlalchemy import Pagination
+from sqlalchemy.orm.exc import NoResultFound
+from wtforms.ext.sqlalchemy.orm import model_form, converts, ModelConverter
+from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from wtforms import fields as wtf_fields
+from wtforms import widgets, validators
+from wtforms.ext.sqlalchemy import fields as sa_fields
 from wtforms.form import Form
 
-from webhelpers.paginate import Page
 
-
-def Admin(models, model_forms={}, include_models=[], exclude_models=[], admin_db_session=None, exclude_pk=True):
+def Admin(models, model_forms={}, include_models=[], exclude_models=[], exclude_pks=False, admin_db_session=None):
     global model_dict
     global form_dict
     global db_session
-    
+
     try:
         model_dict
     except NameError:
@@ -39,22 +43,22 @@ def Admin(models, model_forms={}, include_models=[], exclude_models=[], admin_db
         if i in exclude_models:
             raise "'%s' is in both include_models and exclude_models" % i
 
-    #XXX: fix base handling
+    #XXX: fix base handling so it will work with non-Declarative models
     if type(models) == types.ModuleType:
         if include_models:
             for model in include_models:
                 module_dict = models.__dict__
-                if model in module_dict and isinstance(module_dict[model], sqlalchemy.ext.declarative.DeclarativeMeta):
+                if model in module_dict and isinstance(module_dict[model], sa.ext.declarative.DeclarativeMeta):
                     model_dict[model] = module_dict[model]
         else:
-            model_dict = dict([(k,v) for k,v in models.__dict__.items() if isinstance(v, sqlalchemy.ext.declarative.DeclarativeMeta) and k != 'Base'])
+            model_dict = dict([(k,v) for k,v in models.__dict__.items() if isinstance(v, sa.ext.declarative.DeclarativeMeta) and k != 'Base'])
 
     if model_dict:
-        form_dict = dict([(k, _form_for_model(v, exclude_pk=exclude_pk)) for k,v in model_dict.items()])
+        form_dict = dict([(k, _form_for_model(v, exclude_pk=exclude_pks)) for k,v in model_dict.items()])
         for model, form in model_forms.items():
             if model in form_dict:
                 form_dict[model] = form
-                
+
     return admin
 
 
@@ -64,33 +68,28 @@ admin = Module(__name__)
 @admin.route('/')
 def index():
     """
-    Admin module views. List available models/tables for this user to 
+    Admin module views. List available models/tables for this user to
     perform CUID
     """
     return render_template('admin/index.html',
-                           admin_models=model_dict.keys())
+                           admin_models=sorted(model_dict.keys()))
 
 
 @admin.route('/list/<model_name>/')
-def generic_model_list(model_name, page=1):
+def generic_model_list(model_name):
     if not model_name in model_dict.keys():
         return "%s cannot be accessed through this admin page" % (model_name,)
     model = model_dict[model_name]
-    _paginate = dict()
-    def get_page_url(page, partial=None):
-        url = url_for('generic_model_list', model_name = model_name, page = page,)
-        if partial:
-            url += "&partial=1"
-        return url
-    _paginate['url'] = get_page_url
     model_instances = model.query
-    page = request.args.get('page','1')
-    paged_instance = Page(model_instances, page=page, items_per_page=15, **_paginate)  
+    per_page = 15
+    page = int(request.args.get('page','1'))
+    items = model_instances.limit(per_page).offset((page - 1) * per_page).all()
+    pagination = Pagination(model_instances, page, per_page, model_instances.count(), items)
     return render_template('admin/list.html',
-                           _get_primary_key=_get_primary_key,
-                           model_instances=paged_instance,
+                           _get_pk_value=_get_pk_value,
+                           model_instances=pagination.items,
                            model_name=model_name,
-                           pagine_string = paged_instance.pager(),
+                           pagination=pagination,
                            )
 
 
@@ -100,19 +99,18 @@ def generic_model_add(model_name):
         return "%s cannot be accessed through this admin page" % (model_name,)
 
     model = model_dict[model_name]
-    form_for_model = form_dict[model_name]
+    model_form = form_dict[model_name]
     model_instance = model()
 
     if request.method == 'GET':
-        form = form_for_model()
+        form = model_form()
         return render_template('admin/add.html',
                                model_name=model_name,
                                form=form)
-
     elif request.method == 'POST':
-        form = form_for_model(request.form)
+        form = model_form(request.form)
         if form.validate():
-            form.populate_obj(model_instance)        
+            model_instance = _populate_model_from_form(model_instance, form)
             db_session.add(model_instance)
             db_session.commit()
             flash('%s added: %s' % (model_name, model_instance), 'success')
@@ -132,7 +130,7 @@ def generic_model_delete(model_name, model_key):
 
     model = model_dict[model_name]
 
-    pk = _get_primary_key_column(model).key
+    pk = _get_pk_name(model)
     pk_query_dict = {pk: model_key}
 
     try:
@@ -155,9 +153,9 @@ def generic_model_edit(model_name, model_key):
     model = model_dict[model_name]
     model_form = form_dict[model_name]
 
-    pk = _get_primary_key_column(model).key
+    pk = _get_pk_name(model)
     pk_query_dict = {pk: model_key}
-    
+
     try:
         model_instance = model.query.filter_by(**pk_query_dict).one()
     except NoResultFound:
@@ -174,7 +172,7 @@ def generic_model_edit(model_name, model_key):
         form = model_form(request.form, obj=model_instance)
 
         if form.validate():
-            form.populate_obj(model_instance)
+            model_instance = _populate_model_from_form(model_instance, form)
             db_session.add(model_instance)
             db_session.commit()
             flash('%s updated: %s' % (model_name, model_instance), 'success')
@@ -189,59 +187,58 @@ def generic_model_edit(model_name, model_key):
 
 
 
-def _get_primary_key(model):
+def _populate_model_from_form(model_instance, form):
     """
-    Return the primary key value for a given model. Assumes single
-    primary key.
+    Return a populated model instance from a form.
     """
-    return model.__dict__[_get_primary_key_column(model).key]
+    for name, field in form._fields.iteritems():
+        field.populate_obj(model_instance, name)
+
+    return model_instance
 
 
-def _get_primary_key_column(model):
+
+def _get_pk_value(model_instance):
     """
-    Return the primary key column for a given model. Assumes single
-    primary key.
+    Return the primary key value for a given model_instance
+    instance. Assumes single primary key.
     """
-    table = model.__table__
-    if len(table.primary_key) > 1:
-        return NotImplementedError, "This admin doesn't work with multiple-column primary keys"
-    
-    for primary_key in table.primary_key:
-        return primary_key
+    return getattr(model_instance, _get_pk_name(model_instance))
 
 
-def _get_model_from_column(column):
-    for model in model_dict.values():
-        if column.name in model.__table__.c:
-            return model
+def _get_pk_name(model):
+    """
+    Return the primary key attribute name for a given model (either
+    instance or class). Assumes single primary key.
+    """
+    model_mapper =  model.__mapper__
+
+    for prop in model_mapper.iterate_properties:
+        if isinstance(prop, sa.orm.properties.ColumnProperty) and prop.columns[0].primary_key:
+            return prop.key
+
+    return None
 
 
-def _form_for_model(model_class, exclude_pk=False):
+def _form_for_model(model_class, exclude=[], exclude_pk=False):
     """
     Return a form for a given model. This will be a form generated by
     wtforms.ext.sqlalchemy.model_form, but decorated with a
     QuerySelectField for foreign keys.
     """
-    exclude=[]
+    model_mapper = sa.orm.class_mapper(model_class)
+
+    relationship_fields = []
+
     if exclude_pk:
-        exclude=[_get_primary_key_column(model_class).name]
-    
-    form = model_form(model_class, exclude=exclude)
+        exclude.append(_get_pk_name(model_class))
 
-    for foreign_key in model_class.__table__.foreign_keys:
-        foreign_model = _get_model_from_column(foreign_key.column)
-        
-        if 'descriptive_key' in model_class.__dict__:
-            label_key = foreign_model.descriptive_key.fget
-        else:
-            label_key = foreign_model.__repr__
+    # exclude any foreign_keys that we have relationships for;
+    # relationships will be mapped to select fields by the
+    # AdminConverter
+    exclude.extend([relationship.local_side[0].name for relationship in model_mapper.iterate_properties if isinstance(relationship, sa.orm.properties.RelationshipProperty)])
 
-        select_field = QuerySelectFieldAsPK(foreign_model.__name__,
-                                            query_factory=_query_factory_for(foreign_model),
-                                            allow_blank=False,
-                                            get_label=label_key)
-
-        setattr(form, foreign_key.parent.name, select_field)
+    form = model_form(model_class, exclude=exclude, converter=AdminConverter())
 
     return form
 
@@ -253,41 +250,124 @@ def _query_factory_for(model_class):
     for QuerySelectFields.
     """
     def query_factory():
-        return model_class.query.all()
+        return sorted(model_class.query.all(), key=repr)
 
     return query_factory
 
 
-
-# -----------------------------------------------------------------------------
-# Special fields
-# -----------------------------------------------------------------------------
-class QuerySelectFieldAsPK(QuerySelectField):
+class DateTimePickerWidget(widgets.TextInput):
     """
-    A modified QuerySelectField that returns the primary key of an
-    object rather than the SQLAlchemy instance. This makes it possible
-    to run form.populate_obj(model_instance) without trouble since the
-    primary key is used rather than the model instance itself. There
-    may be a better way to do this - but it works for now.
+    TextInput widget that adds a 'datetimepicker' class to the html
+    input element; this makes it easy to write a jQuery selector that
+    adds a UI widget for datetime picking.
     """
-    def post_validate(self, form, stop_validation):
-        """
-        After validation, switch the model instance into its primary
-        key primary_key.
-        """
-        get_pk = self.get_pk
-        self.data = get_pk(self.data)
+    def __call__(self, field, **kwargs):
+        c = kwargs.pop('class', '') or kwargs.pop('class_', '')
+        kwargs['class'] = u'datetimepicker %s' % c
+        return super(DateTimePickerWidget, self).__call__(field, **kwargs)
 
-    def iter_choices(self):
-        """
-        This overrides QuerySelectField's iter_choices to account for
-        making the comparison on the primary_key so that when
-        rendering the html widgets, the correct value starts off as
-        selected.
-        """
-        if self.allow_blank:
-            yield (u'__None', self.blank_text, self.data is None)
 
-        get_pk = self.get_pk
-        for pk, obj in self._get_object_list():
-            yield (pk, self.get_label(obj), unicode(get_pk(obj)) == unicode(self.data))
+class DatePickerWidget(widgets.TextInput):
+    """
+    TextInput widget that adds a 'datepicker' class to the html input
+    element; this makes it easy to write a jQuery selector that adds a
+    UI widget for date picking.
+    """
+    def __call__(self, field, **kwargs):
+        c = kwargs.pop('class', '') or kwargs.pop('class_', '')
+        kwargs['class'] = u'datepicker %s' % c
+        return super(DatePickerWidget, self).__call__(field, **kwargs)
+
+
+class AdminConverter(ModelConverter):
+    """
+    Subclass of the wtforms sqlalchemy Model Converter that handles
+    relationship properties and uses custom widgets for date and
+    datetime objects.
+    """
+
+    def convert(self, model, mapper, prop, field_args):
+        if not isinstance(prop, sa.orm.properties.ColumnProperty) and not isinstance(prop, sa.orm.properties.RelationshipProperty):
+            # XXX We don't support anything but ColumnProperty and
+            # RelationshipProperty at the moment.
+            return
+
+        if isinstance(prop, sa.orm.properties.ColumnProperty):
+            if len(prop.columns) != 1:
+                raise TypeError('Do not know how to convert multiple-column properties currently')
+
+            column = prop.columns[0]
+
+            kwargs = {
+                'validators': [],
+                'filters': [],
+                'default': column.default,
+            }
+            if field_args:
+                kwargs.update(field_args)
+
+            if column.nullable:
+                kwargs['validators'].append(validators.Optional())
+
+            if self.use_mro:
+                types = inspect.getmro(type(column.type))
+            else:
+                types = [type(column.type)]
+
+            converter = None
+            for col_type in types:
+                type_string = '%s.%s' % (col_type.__module__, col_type.__name__)
+                if type_string.startswith('sqlalchemy'):
+                    type_string = type_string[11:]
+
+                if type_string in self.converters:
+                    converter = self.converters[type_string]
+                    break
+            else:
+                for col_type in types:
+                    if col_type.__name__ in self.converters:
+                        converter = self.converters[col_type.__name__]
+                        break
+                else:
+                    return
+            return converter(model=model, mapper=mapper, prop=prop, column=column, field_args=kwargs)
+
+
+        if isinstance(prop, sa.orm.properties.RelationshipProperty):
+            if prop.direction == sa.orm.interfaces.MANYTOONE and len(prop.local_remote_pairs) != 1:
+                raise TypeError('Do not know how to convert multiple-column properties currently')
+
+            elif prop.direction == sa.orm.interfaces.MANYTOMANY and len(prop.local_remote_pairs) != 2:
+                raise TypeError('Do not know how to convert multiple-column properties currently')
+
+            local_column = prop.local_remote_pairs[0][0]
+            foreign_model = prop.mapper.class_
+
+            if prop.direction == sa.orm.properties.MANYTOONE:
+                return QuerySelectField(
+                    foreign_model.__name__,
+                    query_factory=_query_factory_for(foreign_model),
+                    allow_blank=local_column.nullable)
+
+            if prop.direction == sa.orm.properties.MANYTOMANY:
+                return sa_fields.QuerySelectMultipleField(
+                    foreign_model.__name__,
+                    query_factory=_query_factory_for(foreign_model),
+                    allow_blank=local_column.nullable)
+
+
+    @converts('Date')
+    def conv_Date(self, field_args, **extra):
+        field_args['widget'] = DatePickerWidget()
+        return wtf_fields.DateField(**field_args)
+
+    @converts('DateTime')
+    def conv_DateTime(self, field_args, **extra):
+        # XXX: should show disabled (greyed out) w/current value,
+        #      indicating it is updated internally?
+        if field_args['default']:
+            if inspect.isfunction(field_args['default'].arg):
+                return None
+        field_args['widget'] = DateTimePickerWidget()
+        return wtf_fields.DateTimeField(**field_args)
+
