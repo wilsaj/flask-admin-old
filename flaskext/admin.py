8c87435564d634237783cdf510151cbe0eb7ec02
Andy Wilson <wilson.andrew.j@gmail.com>
2011-03-16 12:07:21 -0500

    more cleanup in import

--- flaskext/admin.py
+++ flaskext/admin.py
@@ -15,7 +15,8 @@ import types
 
 import sqlalchemy as sa
 import sqlalchemy.ext.declarative
-from flask import app, flash, g, Module, render_template, redirect, request, session, url_for
+from flask import app, flash, g, Module, render_template, redirect, \
+     request, session, url_for
 from flaskext.sqlalchemy import Pagination
 from sqlalchemy.orm.exc import NoResultFound
 from wtforms import widgets, validators
