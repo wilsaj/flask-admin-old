cd8bbe1fa8e1b83db64431bf55fc5883e1235994
andy wilson <wilson.andrew.j+github@gmail.com>
2011-05-31 07:05:59 -0500

    beginnings of an auth example

---
+++ example/authentication/authentication.py
@@ -0,0 +1,129 @@
+from functools import wraps
+import sys
+
+from flask import Flask, g, redirect, request, url_for
+from flaskext import themes
+from flaskext import admin
+from sqlalchemy import create_engine, Table
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Text, String, Float, Time, Enum
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import ForeignKey
+from flaskext.wtf import Form, TextField
+
+app = Flask(__name__)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///simple.db'
+app.config['SECRET_KEY'] = 'not secure'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+
+
+# ----------------------------------------------------------------------
+# Association tables
+# ----------------------------------------------------------------------
+course_student_association_table = Table(
+    'course_student_association',
+    Base.metadata,
+    Column('student_id', Integer, ForeignKey('student.id')),
+    Column('course_id', Integer, ForeignKey('course.id')))
+
+
+# ----------------------------------------------------------------------
+# Models
+# ----------------------------------------------------------------------
+class Course(Base):
+    __tablename__ = 'course'
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String)
+    teacher_id = Column(Integer, ForeignKey('teacher.id'), nullable=False)
+    start_time = Column(Time)
+    end_time = Column(Time)
+
+    teacher = relationship('Teacher', backref='courses')
+    students = relationship('Student',
+                            secondary=course_student_association_table,
+                            backref='courses')
+    # teacher = relation()
+    # students = relation()
+
+    def __repr__(self):
+        return self.subject
+
+
+class Student(Base):
+    __tablename__ = 'student'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), unique=True)
+
+    def __repr__(self):
+        return self.name
+
+
+class Teacher(Base):
+    __tablename__ = 'teacher'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), unique=True)
+
+    def __repr__(self):
+        return self.name
+
+
+themes.setup_themes(app)
+admin_mod = admin.Admin(sys.modules[__name__], admin_db_session=db_session,
+                        exclude_pks=True)
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        g.user = form.username
+        redirect(request.args['next'])
+    else:
+        return render_template("upload.html",
+                               form=form)
+
+@admin_mod.before_request
+def check_password():
+    try:
+        if g.user is None:
+            return redirect(url_for('with_authentication.login', next=request.url))
+    except AttributeError:
+        return redirect(url_for('with_authentication.login', next=request.url))
+
+app.register_module(admin_mod, url_prefix='/admin')
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+
+
+
+
+class LoginForm(Form):
+    username = TextField('username')
+    password = TextField('password')
+
+
+
+
+@app.route('/')
+def go_to_admin():
+    return redirect('/admin')
+
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
+    app.run(debug=True)
