5509081b3400f67a10aadd322efd48083b348200
andy wilson <wilson.andrew.j+github@gmail.com>
2011-06-26 23:28:09 -0500

    remove old comment todo

--- example/flask-sqlalchemy/flaskext_sa_simple.py
+++ example/flask-sqlalchemy/flaskext_sa_simple.py
@@ -32,8 +32,6 @@ class Course(db.Model):
     students = db.relationship('Student',
                                secondary=course_student_association_table,
                                backref='courses')
-    # teacher = relation()
-    # students = relation()
 
     def __repr__(self):
         return self.subject
