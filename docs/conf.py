8b6eca75c5d84100b3e3b3386118fb7902b99913
wilsaj <wilson.andrew.j+github@gmail.com>
2011-07-12 10:58:16 -0500

    docs: fix broken github 'fork me' link

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,9 @@ html_theme = 'flask_small'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'index_logo': False}
+html_theme_options = {'index_logo': False,
+                      'github_fork': 'wilsaj/flask-admin'
+                      }
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
