d9e3ae8be399e9d257ce81a3f9a2f87572c853f1
wilsaj <wilson.andrew.j+github@gmail.com>
2012-04-12 18:02:01 -0500

    prepare for 0.4.2 bugfix release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Admin',
-    version='0.4.2-dev',
+    version='0.4.2',
     url='https://github.com/wilsaj/flask-admin/',
     license='BSD',
     author='Andy Wilson',
