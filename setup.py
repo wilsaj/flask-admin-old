0af052b32064414f0dd9ad212b9c063a6ab4625e
wilsaj <wilson.andrew.j+github@gmail.com>
2012-04-12 17:04:39 -0500

    version bump to 0.4.2-dev

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Admin',
-    version='0.4.1',
+    version='0.4.2-dev',
     url='https://github.com/wilsaj/flask-admin/',
     license='BSD',
     author='Andy Wilson',
